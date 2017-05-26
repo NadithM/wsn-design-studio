32d114b16646291a331a8cb6b4516b754376b689
Asela Dasanayaka <rukmal.tb@gmail.com>
2017-05-26 15:23:59 +0530

    code download completed

--- src/main/java/com/fyp/wsn/Controller/RouteController.java
+++ src/main/java/com/fyp/wsn/Controller/RouteController.java
@@ -1,7 +1,10 @@
 package com.fyp.wsn.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Asela on 5/3/2017.
@@ -34,6 +37,12 @@ public class RouteController {
     @RequestMapping("/create/microcontroller")
     public String addMicrocontroller(){
         return "addMicrocontroller";
+    }
+
+    @RequestMapping(value="/code/{nodes}" ,method = RequestMethod.GET)
+    public String code(@PathVariable("nodes") String[] nodes, Model model){
+        model.addAttribute(nodes);
+        return "code";
     }
 
 }
